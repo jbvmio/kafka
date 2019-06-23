2a2c7ae697af247d09e075c7c4b464cc5d0e2f5f
Jimmy Bonds <jbonds@jbvm.io>
2019-06-22 21:19:51 -0400

    initial completion of AsyncProducer

--- produce.go
+++ produce.go
@@ -2,6 +2,7 @@ package kafka
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/Shopify/sarama"
@@ -12,6 +13,33 @@ import (
 type ProducerError struct {
 	Msg *Message
 	Err error
+}
+
+func (e *ProducerError) toSarama() *sarama.ProducerError {
+	return &sarama.ProducerError{
+		Msg: e.Msg.toSarama(),
+		Err: e.Err,
+	}
+}
+
+func convertProducerErr(e *sarama.ProducerError) *ProducerError {
+	return &ProducerError{
+		Msg: convertProducerMsg(e.Msg),
+		Err: e.Err,
+	}
+}
+
+func convertProducerMsg(m *sarama.ProducerMessage) (msg *Message) {
+	k, _ := m.Key.Encode()
+	v, _ := m.Value.Encode()
+	return &Message{
+		Key:       k,
+		Value:     v,
+		Topic:     m.Topic,
+		Partition: m.Partition,
+		Offset:    m.Offset,
+		Timestamp: m.Timestamp,
+	}
 }
 
 // SendMSG sends a message to the targeted topic/partition defined in the message.
@@ -44,8 +72,11 @@ func (kc *KClient) SendMessages(messages []*Message) (err error) {
 type Producer struct {
 	producer         sarama.AsyncProducer
 	cl               *KClient
+	killChan         chan struct{}
 	errors           chan *ProducerError
 	input, successes chan *Message
+	wg               sync.WaitGroup
+	noClose          bool
 }
 
 // NewProducer returns a new Producer with Successes and Error channels that must be read from.
@@ -69,9 +100,12 @@ func NewProducer(addrs []string, config *sarama.Config) (*Producer, error) {
 	}
 	producer.producer = p
 	producer.cl = kc
+	producer.killChan = make(chan struct{})
 	producer.errors = make(chan *ProducerError)
 	producer.input = make(chan *Message)
 	producer.successes = make(chan *Message)
+	producer.wg = sync.WaitGroup{}
+	go producer.start()
 	return &producer, nil
 }
 
@@ -88,13 +122,18 @@ func (kc *KClient) NewProducer(addrs []string, config *sarama.Config) (*Producer
 	if err != nil {
 		return &Producer{}, err
 	}
-	return &Producer{
+	producer := Producer{
 		producer:  p,
 		cl:        kc,
+		killChan:  make(chan struct{}),
 		errors:    make(chan *ProducerError),
 		input:     make(chan *Message),
 		successes: make(chan *Message),
-	}, nil
+		wg:        sync.WaitGroup{},
+		noClose:   true,
+	}
+	go producer.start()
+	return &producer, nil
 }
 
 // RequiredAcks sets the desired number of replica acknowledgements it must see from the broker
@@ -148,31 +187,43 @@ func (p *Producer) Close() error {
 }
 
 // Shutdown starts the shutdown of the producer, draining both Errors and Successes channels, then closes the underlying client.
-// A given timeout value (in seconds) can be used to specify an allotted time for draining the channels.
+// A given timeout value (in seconds) can be used to specify an allotted time for draining the channels. (default: 10)
 // If the timer expires and error will be returned.
-func (p *Producer) Shutdown(timeout int) error {
+func (p *Producer) Shutdown(timeout ...int) error {
+	var to int
+	switch {
+	case len(timeout) == 1:
+		to = timeout[0]
+	default:
+		to = 10
+	}
 	var errd error
-	var closed bool
+	doneMap := make(map[uint8]bool, 2)
+	close(p.killChan)
 	p.producer.AsyncClose()
-	timer := time.NewTicker(time.Duration(timeout) * time.Second)
+	timer := time.NewTicker(time.Duration(to) * time.Second)
 drainLoop:
 	for {
 		select {
 		case <-timer.C:
-			err := p.producer.Close()
-			if err != nil {
-				errd = fmt.Errorf("Timed out Draining Producer. Error Closing Client: %v", err)
-			} else {
-				closed = true
-				errd = fmt.Errorf("Timed out Draining Producer")
-			}
+			errd = fmt.Errorf("Timed out Draining Producer")
 			break drainLoop
-		case <-p.producer.Successes():
-		case <-p.producer.Errors():
+		case _, ok := <-p.producer.Successes():
+			if !ok {
+				doneMap[1] = true
+			}
+		case _, ok := <-p.producer.Errors():
+			if !ok {
+				doneMap[2] = true
+			}
+		default:
+			if doneMap[1] && doneMap[2] {
+				break drainLoop
+			}
 		}
 	}
-	if !closed {
-		errd = p.producer.Close()
+	if !p.noClose {
+		p.cl.Close()
 	}
 	return errd
 }
@@ -180,5 +231,49 @@ drainLoop:
 // Input is the input channel for the user to write messages to that they
 // wish to send.
 func (p *Producer) Input() chan<- *Message {
-	return p.producer.Input()
+	return p.input
+}
+
+// Successes is the success output channel back to the user when Return.Successes is
+// enabled. If Return.Successes is true, you MUST read from this channel or the
+// Producer will deadlock. It is suggested that you send and read messages
+// together in a single select statement.
+func (p *Producer) Successes() chan<- *Message {
+	return p.successes
+}
+
+// Errors is the error output channel back to the user. You MUST read from this
+// channel or the Producer will deadlock when the channel is full. Alternatively,
+// you can set Producer.Return.Errors in your config to false, which prevents
+// errors to be returned.
+func (p *Producer) Errors() chan<- *ProducerError {
+	return p.errors
+}
+
+func (p *Producer) start() {
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for m := range p.producer.Errors() {
+			p.errors <- convertProducerErr(m)
+		}
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for m := range p.producer.Successes() {
+			p.successes <- convertProducerMsg(m)
+		}
+	}()
+inputLoop:
+	for {
+		select {
+		case <-p.killChan:
+			break inputLoop
+		case m := <-p.input:
+			p.producer.Input() <- m.toSarama()
+		}
+	}
+	wg.Wait()
 }
