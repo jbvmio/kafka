b95d78d7eb64f024c294ede2a69cc931fdc86909
Jimmy Bonds <jbonds@jbvm.io>
2019-10-10 10:44:41 -0400

    update api versions

--- metadata_cluster.go
+++ metadata_cluster.go
@@ -107,49 +107,51 @@ func (kc *KClient) ReqMetadata() (*sarama.MetadataResponse, error) {
 
 // APIKey Codes
 const (
-	APIKeyProduce                 int16 = 0
-	APIKeyFetch                   int16 = 1
-	APIKeyListOffsets             int16 = 2
-	APIKeyMetadata                int16 = 3
-	APIKeyLeaderAndIsr            int16 = 4
-	APIKeyStopReplica             int16 = 5
-	APIKeyUpdateMetadata          int16 = 6
-	APIKeyControlledShutdown      int16 = 7
-	APIKeyOffsetCommit            int16 = 8
-	APIKeyOffsetFetch             int16 = 9
-	APIKeyFindCoordinator         int16 = 10
-	APIKeyJoinGroup               int16 = 11
-	APIKeyHeartbeat               int16 = 12
-	APIKeyLeaveGroup              int16 = 13
-	APIKeySyncGroup               int16 = 14
-	APIKeyDescribeGroups          int16 = 15
-	APIKeyListGroups              int16 = 16
-	APIKeySaslHandshake           int16 = 17
-	APIKeyApiVersions             int16 = 18
-	APIKeyCreateTopics            int16 = 19
-	APIKeyDeleteTopics            int16 = 20
-	APIKeyDeleteRecords           int16 = 21
-	APIKeyInitProducerId          int16 = 22
-	APIKeyOffsetForLeaderEpoch    int16 = 23
-	APIKeyAddPartitionsToTxn      int16 = 24
-	APIKeyAddOffsetsToTxn         int16 = 25
-	APIKeyEndTxn                  int16 = 26
-	APIKeyWriteTxnMarkers         int16 = 27
-	APIKeyTxnOffsetCommit         int16 = 28
-	APIKeyDescribeAcls            int16 = 29
-	APIKeyCreateAcls              int16 = 30
-	APIKeyDeleteAcls              int16 = 31
-	APIKeyDescribeConfigs         int16 = 32
-	APIKeyAlterConfigs            int16 = 33
-	APIKeyAlterReplicaLogDirs     int16 = 34
-	APIKeyDescribeLogDirs         int16 = 35
-	APIKeySaslAuthenticate        int16 = 36
-	APIKeyCreatePartitions        int16 = 37
-	APIKeyCreateDelegationToken   int16 = 38
-	APIKeyRenewDelegationToken    int16 = 39
-	APIKeyExpireDelegationToken   int16 = 40
-	APIKeyDescribeDelegationToken int16 = 41
-	APIKeyDeleteGroups            int16 = 42
+	APIKeyProduce int16 = iota
+	APIKeyFetch
+	APIKeyListOffsets
+	APIKeyMetadata
+	APIKeyLeaderAndIsr
+	APIKeyStopReplica
+	APIKeyUpdateMetadata
+	APIKeyControlledShutdown
+	APIKeyOffsetCommit
+	APIKeyOffsetFetch
+	APIKeyFindCoordinator
+	APIKeyJoinGroup
+	APIKeyHeartbeat
+	APIKeyLeaveGroup
+	APIKeySyncGroup
+	APIKeyDescribeGroups
+	APIKeyListGroups
+	APIKeySaslHandshake
+	APIKeyApiVersions
+	APIKeyCreateTopics
+	APIKeyDeleteTopics
+	APIKeyDeleteRecords
+	APIKeyInitProducerId
+	APIKeyOffsetForLeaderEpoch
+	APIKeyAddPartitionsToTxn
+	APIKeyAddOffsetsToTxn
+	APIKeyEndTxn
+	APIKeyWriteTxnMarkers
+	APIKeyTxnOffsetCommit
+	APIKeyDescribeAcls
+	APIKeyCreateAcls
+	APIKeyDeleteAcls
+	APIKeyDescribeConfigs
+	APIKeyAlterConfigs
+	APIKeyAlterReplicaLogDirs
+	APIKeyDescribeLogDirs
+	APIKeySaslAuthenticate
+	APIKeyCreatePartitions
+	APIKeyCreateDelegationToken
+	APIKeyRenewDelegationToken
+	APIKeyExpireDelegationToken
+	APIKeyDescribeDelegationToken
+	APIKeyDeleteGroups
+	APIKeyElectPreferredLeaders
+	APIKeyIncrementalAlterConfigs
 )
 
 // APIKey Descriptions
@@ -197,6 +199,8 @@ var APIDescriptions = map[int16]string{
 	APIKeyExpireDelegationToken:   "ExpireDelegationToken",
 	APIKeyDescribeDelegationToken: "DescribeDelegationToken",
 	APIKeyDeleteGroups:            "DeleteGroups",
+	APIKeyElectPreferredLeaders:   "ElectPreferredLeaders",
+	APIKeyIncrementalAlterConfigs: "IncrementalAlterConfigs",
 }
 
 // Kafka API Versions:
